28cb312b27065ea2a47b37aed8cd91716fac8d6d
jcerb <brt.jst@gmail.com>
2021-04-28 17:36:42 -0700

    setup models

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: {type: String},
+  image: {type: [String] },
+  link: {type: String, required: true},
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
