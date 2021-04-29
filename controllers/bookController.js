406e8cadd98f5d07c3bbc08a4cb600fe2ebd40cd
jcerb <brt.jst@gmail.com>
2021-04-28 18:26:55 -0700

    add bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,22 @@
 const db = require("../models");
 
 // define methods for the bookController
+module.exports = {
+  findAll: function (req, res) {
+    db.Book.find(req.query)
+      .sort({ title })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  save: function (req, res) {
+    db.Book.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  delete: function (req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
--- models/book.js
+++ models/book.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: {type: String},
-  image: {type: [String] },
-  link: {type: String, required: true},
+  description: { type: String },
+  image: { type: [String] },
+  link: { type: String, required: true },
 });
 
 const Post = mongoose.model("Post", postSchema);
